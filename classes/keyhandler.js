3db5a34d3d39ba9442caa60bb4eb419cb87ac3aa
rudolfp <capacopx@gmail.com>
2025-09-08 22:27:47 +0200

    keyhandler refactor

--- classes/keyhandler.js
+++ classes/keyhandler.js
@@ -2,111 +2,94 @@ export function hello () {
     console.log("hello from keyhandler.js");
 }
 
-export class KeyHandler {
-    #onTrigger;
-    #onRelease;
-    #context;
-
-    constructor (keyCode, context = undefined) {
-        this.keyCode = keyCode;
-        this.#onTrigger = new Set();
-        this.#onRelease = new Set();
-
-        this.#context = context ? context : window;
-    }
-
-    addTriggerListener (callbackFn) {
-        this.#context.addEventListener("keydown", 
-            (event) => {
-                if (event.code == this.keyCode && !event.repeat) {
-                    callbackFn(event);
-                }
-            }
-        );
-    }
-
-    addHoldListener (callbackFn) {
-        this.#context.addEventListener("keydown", 
-            (event) => {
-                if (event.code == this.keyCode) {
-                    callbackFn(event);
-                }
-            }
-        );
-    }
-
-    addReleaseListener (callbackFn) {
-        this.#context.addEventListener("keyup", 
-            (event) => {
-                if (event.code === this.keyCode) {
-                    callbackFn(event);
-                }
-            }
-        );
-    }
-}
-
 export default class KeyboardHandler {
-    #context
-    #keyHandlers
+    #keys
     #keysPressed
-    #keysHeld
+    #context
 
     constructor (context = undefined) {
-        this.#keyHandlers = new Map();
+        this.#keys = new Map();
         this.#keysPressed = new Set();
-        this.#keysHeld = new Set();
 
         this.#context = context ? context : window;
+        this.#context.addEventListener("keydown", this.#handleKeydown); // TODO: understand 'this'
+        this.#context.addEventListener("keyup", this.#handleKeyup); // TODO: understand 'this'
     }
 
     get keysPressed () { return this.#keysPressed; }
 
-    get keysHeld () { return this.#keysHeld; }
+    #assertKeyInKeys (keyCode) {
+        if (!this.#keys.has(keyCode)) throw new Error(`'${keyCode}' not in #keys`);
+    }
 
     addKeys (...keyCodes) {
         keyCodes.forEach((keyCode) => {
-            this.#keyHandlers.set(keyCode, new KeyHandler(keyCode, this.#context));
+            const keyObject = {
+                onTrigger: new Set(),
+                onHold: new Set(),
+                onRelease: new Set()
+            }
+
+            this.#keys.set(keyCode, keyObject);
         });
     }
 
     addTriggerListener (keyCode, callbackFn) {
-        this.#keyHandlers.get(keyCode).addTriggerListener((event) => {
-            this.#keysPressed.add(keyCode);
-            callbackFn(event);
-        });
-    }
-
-    addTriggerListenerAll (callbackFn) {
-        this.#keyHandlers.forEach((keyHandler, keyCode) => {
-            this.addTriggerListener(keyCode, callbackFn);
-        });
+        this.#assertKeyInKeys(keyCode);
+        this.#keys.get(keyCode).onTrigger.add(callbackFn);
     }
 
     addHoldListener (keyCode, callbackFn) {
-        this.#keyHandlers.get(keyCode).addHoldListener((event) => {
-            this.#keysHeld.add(keyCode);
-            callbackFn(event);
+        this.#assertKeyInKeys(keyCode);
+        this.#keys.get(keyCode).onHold.add(callbackFn);
+    }
+
+    addReleaseListener (keyCode, callbackFn) {
+        this.#assertKeyInKeys(keyCode);
+        this.#keys.get(keyCode).onRelease.add(callbackFn);
+    }
+
+    addTriggerListenerAll (callbackFn) {
+        this.#keys.forEach((keyObject) => {
+            keyObject.onTrigger.add(callbackFn);
         });
     }
 
     addHoldListenerAll (callbackFn) {
-        this.#keyHandlers.forEach((keyHandler, keyCode) => {
-            this.addHoldListener(callbackFn);
-        });
-    }
-
-    addReleaseListener (keyCode, callbackFn) {
-        this.#keyHandlers.get(keyCode).addReleaseListener((event) => {
-            this.#keysPressed.delete(keyCode);
-            if (this.#keysHeld.has(keyCode)) this.#keysHeld.delete(keyCode);
-            callbackFn(event);
+        this.#keys.forEach((keyObject) => {
+            keyObject.onHold.add(callbackFn);
         });
     }
 
     addReleaseListenerAll (callbackFn) {
-        this.#keyHandlers.forEach((keyHandler, keyCode) => {
-            this.addReleaseListener(keyCode, callbackFn);
+        this.#keys.forEach((keyObject) => {
+            keyObject.onRelease.add(callbackFn);
         });
+    }
+
+    #handleKeydown = (event) => {
+        const keyCode = event.code;
+
+        const keyObject = this.#keys.get(keyCode);
+        if (!keyObject) return;
+
+        this.#keysPressed.add(keyCode);
+
+        if (!event.repeat) {
+            keyObject.onTrigger.forEach(callbackFn => callbackFn(event));
+        }
+
+        keyObject.onHold.forEach(callbackFn => callbackFn(event));
+    }
+
+    #handleKeyup = (event) => {
+        const keyCode = event.code;
+
+        const keyObject = this.#keys.get(keyCode);
+        if (!keyObject) return;
+
+        this.#keysPressed.delete(keyCode);
+
+        keyObject.onRelease.forEach(callbackFn => callbackFn(event));
     }
 }
